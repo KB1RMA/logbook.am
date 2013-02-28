d0884e8d65d7de2eb601097ee4ca86848e60e215
Chris Snyder <kb1rma@arrl.net>
2013-02-27 19:52:02 -0500

    Hopefully more maintainable callsign autocomplete object

--- app/webroot/js/main.js
+++ app/webroot/js/main.js
@@ -11,12 +11,6 @@ if (!$.support.transition)
 	
 	var $ = window.jQuery, 
 			$body = $('body'),
-			$resultsTable = null,
-			$resultsContainer = null,
-			$callsignInput = null,
-			$callsignFind = null,
-			$searchContainer = null,
-			$callSearchContainer = null,
 			$userSettingsDropdown = null,
 			$useMyLocation = null,
 			map_canvas = null,
@@ -93,89 +87,169 @@ if (!$.support.transition)
 	}
 
 	/**
-	 * Grab results from the Callsigns::autocomplete controller
+	 * Object to interact with the auto complete results
 	 */
 
-	function autoComplete( partialCall ) {
+	var autoComplete = {
+		endPoint : '/callsigns/autocomplete',
+		results : null,
+		resultsTable : '',
+		$resultsTable : null,
+		$resultsContainer : null,
+		$callsignInput : null,
+		$callsignFind : null,
+		$searchContainer : null,
+		$callSearchContainer : null,
 
-		var requestData = { 'callsign' : partialCall };
-		
-		$.ajax( {
-				type : 'POST',
-				url : '/callsigns/autocomplete', 
-				data : requestData,
-				dataType : 'json'
-			})
-			.done( function( data, textStatus, jqXHR ) { populateAutoCompleteResults( data ); });
-	}
+		init : function() {
+			// Populate all DOM elements we need to interact with
+			this.$resultsTable = $('#callsign-results table');
+			this.$resultsContainer = $('#callsign-results');
+			this.$callsignInput = $('#callsign-input');
+			this.$callsignFind = $('#callsign-find');
+			this.$searchContainer = $('#callsign-entry');
+			this.$callSearchContainer = $('#call-search');
 
+			// Every time a key is released on the callsign input, autocomplete the results
+			$body.on('keyup', '#callsign-input', function() { 
+				var val = this.value;
 
-	/**
-	 * Populate the autocomplete box with results 
-	 */
+				// Only if it isn't empty
+				if ( val !== '' )
+					autoComplete.find(val); 
+			});
 
-	function populateAutoCompleteResults ( results ) {
-		var i = 0,
-				resultRows = '',
-				anchorTag	= '',
-				yearInPast = new Date();
-				
-		yearInPast.setYear(yearInPast.getFullYear() - 1);
-		yearInPast = Math.round(yearInPast.getTime() / 1000); // Time in epoch
+			// On pageload, if there's something in the callsign input box, populate the results
+			if ( this.$callsignInput.val() )
+				autoComplete.find( this.value );
 
-		// Loop through results		
-		$.each(results.callsigns, function(i, result) {
-			if ( result.person === undefined ) { result.person = {}; }
-			if ( result.address === undefined ) { result.address = {}; }
-			if ( result.qslInfo === undefined ) { result.qslInfo = {}; }
+			// Hide find button if JS is enabled (screw you, poka)
+			autoComplete.$callsignFind.hide();
 
-			var callsign = result.callsign || '',
-			    givenName = result.person.givenName || '',
-			    familyName = result.person.familyName || '',
-			    locality = result.address.locality || '',
-			    region = result.address.region || '',
-          lotw = result.qslInfo.lotwLastActive || '';
+			// When callsign input looses focus, hide the container
+			autoComplete.$callsignInput.blur(function() { 
+				setTimeout( function() { 
+					autoComplete
+						.closeResults()	
+						.resizeCallSearch(); 
+				}, 200 );
+			});
 
-			// An 'active' LOTW user is considered to be someone who has uploaded in the past year
-			if ( lotw > yearInPast ) { lotw = 'LOTW'; } else { lotw = ''; }
+			// When callsign input gains focus, populate results if it's not empty
+			autoComplete.$callsignInput.focus(function() { 
+				autoComplete.find( this.value ); 
+			});	
 
-			anchorTag = '<a href="/call/' + callsign + '/">';		
-			resultRows += '<tr><td>' + anchorTag + callsign + '</a></td>';
-			resultRows += '<td>' + anchorTag + givenName + ' ' + familyName + '</a></td>';
-			resultRows += '<td>' + anchorTag + locality + ', ' + region + '</a></td>';
-			resultRows += '<td>' + lotw + '</a></td></tr>';
-		});
-		
-		// Fade results container out
-		$resultsTable
-			.transition( { opacity : 0 }, 50, function() { 
-				
-				// Reach out to the big bad DOM to populate the results
-				$(this).html(resultRows); 
-				
-				resizeCallSearch(function() { 
-					$resultsContainer.fadeIn(300);
-					$resultsTable.transition( { 'opacity' : 1 }, 50 );	
-				});			
-		});
+			return this;
+		},
 
-	}
+		find : function( partialCall ) {
+			var ajaxOptions = { type     : 'POST',
+													url      : this.endPoint, 
+													data     : { 'callsign' : partialCall },
+													dataType : 'json' };
 
-	function resizeCallSearch( callback ) {
-		var containerHeight = $searchContainer.outerHeight() + $resultsTable.outerHeight();
-		
-		$callSearchContainer.transition( { 'height' : containerHeight }, 100, callback );
+			$.ajax( ajaxOptions )
+				.done( function( data, textStatus, jqXHR ) { 
+					autoComplete.results = data;
+					autoComplete
+						.processResults()
+						.buildResultsTable()
+						.populateResultsTable(); 
+				});
+		},
 
-	}
+		processResults : function() {
+			var processed = [];
 
-	function closeResultsContainer() {
-		$resultsTable.empty();
+			// Loop through JSON containing results		
+			$.each(this.results.callsigns, function(i, result) {
+				if ( result.person  === undefined ) { result.person  = {}; }
+				if ( result.address === undefined ) { result.address = {}; }
+				if ( result.qslInfo === undefined ) { result.qslInfo = {}; }
+					
+				// Build results object
+				processed.push( {	
+					callsign   : result.callsign || '',
+					givenName  : result.person.givenName || '',
+					familyName : result.person.familyName || '',
+					locality   : result.address.locality || '',
+					region     : result.address.region || '',
+					lotw       : result.qslInfo.lotwLastActive || ''
+				});
 
-		if ( ! $body.hasClass('home') )
-			$resultsContainer.fadeOut(300);
+			});
 
-		resizeCallSearch();
-	}
+			this.processedResults = processed;
+
+			return this;
+
+		},
+
+		buildResultsTable : function () {
+			var i = 0,
+			    resultsRows = '',
+			    anchorTag	= '',
+			    yearInPast = new Date(),
+					resultsLength = this.processedResults.length;
+
+			yearInPast.setYear(yearInPast.getFullYear() - 1);
+			yearInPast = Math.round(yearInPast.getTime() / 1000); // Time in epoch
+
+			for ( var i = 0; i < resultsLength; i++ ) {
+				var result = this.processedResults[i],
+				    lotw = '';
+
+				// An 'active' LOTW user is considered to be someone who has uploaded in the past year
+				if ( result.lotw > yearInPast ) { lotw = 'LOTW'; } else { lotw = ''; }
+
+				anchorTag   = '<a href="/call/' + result.callsign + '/">';		
+				resultsRows += '<tr><td>' + anchorTag + result.callsign + '</a></td>';
+				resultsRows += '<td>' + anchorTag + result.givenName + ' ' + result.familyName + '</a></td>';
+				resultsRows += '<td>' + anchorTag + result.locality + ', ' + result.region + '</a></td>';
+				resultsRows += '<td>' + lotw + '</a></td></tr>';
+			}
+
+			this.resultsTable = resultsRows;  
+
+			return this;
+		},
+
+		populateResultsTable : function () {
+					
+			// Fade results container out
+			this.$resultsTable
+				.transition( { opacity : 0 }, 50, function() { 
+					
+					// Reach out to the big bad DOM to populate the results
+					$(this).html(autoComplete.resultsTable); 
+					
+					autoComplete.resizeCallSearch(function() { 
+						autoComplete.$resultsContainer.fadeIn(300);
+						autoComplete.$resultsTable.transition( { 'opacity' : 1 }, 50 );	
+					});			
+			});
+
+			return this;
+
+		},
+
+		resizeCallSearch : function ( callback ) {
+			var containerHeight = this.$searchContainer.outerHeight() + this.$resultsTable.outerHeight();
+			this.$callSearchContainer.transition( { 'height' : containerHeight }, 100, callback );
+		},
+
+		closeResults : function () {
+			this.$resultsTable.empty();
+
+			if ( ! $body.hasClass('home') )
+				this.$resultsContainer.fadeOut(300);
+
+			this.resizeCallSearch();
+		}
+
+	} // End of autoComplete object
+
 	
 	function placeUserOnMap( callback ) {
 			if ( navigator.geolocation ) {
@@ -298,12 +372,6 @@ if (!$.support.transition)
 	function init() {
 
 		$body = $('body');
-		$resultsTable = $('#callsign-results table');
-		$resultsContainer = $('#callsign-results');
-		$callsignInput = $('#callsign-input');
-		$callsignFind = $('#callsign-find');
-		$searchContainer = $('#callsign-entry');
-		$callSearchContainer = $('#call-search');
 		$userSettingsDropdown = $('#user-settings-dropdown');
 		$userSettings = $('#user-settings');
 		map_canvas = document.getElementById("map_canvas");
@@ -314,29 +382,8 @@ if (!$.support.transition)
 		$userSettings.click( function() { toggleSettingsDropdown() } );
 		userPreferences.init();
 		
-		// Every time a key is released on the callsign input, autocomplete the results
-		$body.on('keyup', '#callsign-input', function() { 
-			var val = this.value;
-
-			// Only if it isn't empty
-			if ( val !== '' )
-				autoComplete( val ); 
-		});
-
-		// On pageload, if there's something in the callsign input box, populate the results
-		if ( $callsignInput.val() )
-			autoComplete( this.value );
-
-		// Hide find button if JS is enabled (screw you, poka)
-		$callsignFind.hide();
-
-		// When callsign input looses focus, hide the container
-		$callsignInput.blur(function() { 
-			setTimeout( function() { closeResultsContainer();	resizeCallSearch(); }, 200 );
-		});
-
-		// When callsign input gains focus, populate results if it's not empty
-		$callsignInput.focus(function() { autoComplete( this.value ); });
+		// Initialize Auto call completion
+		autoComplete.init();
 
 		// Enable location button if the BROWSER-EXPERIENCE is adequate (looking at you, Scotty)
 		if(navigator.geolocation) {
