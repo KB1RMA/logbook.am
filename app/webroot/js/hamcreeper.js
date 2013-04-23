729ad0d2ef049615ba4c42fcb818149680a1fac2
Chris Snyder <kb1rma@arrl.net>
2013-04-23 17:07:51 -0400

    Info Window pop ups on map

--- app/webroot/js/hamcreeper.js
+++ app/webroot/js/hamcreeper.js
@@ -1,68 +1,82 @@
-(function (window) {
-  'use strict'
+(function (window, document) {
+    'use strict'
 
-  var
-    $ = window.jQuery,
-    $window = $(window),
-    google = window.google,
-    hams = [],
-    map = null,
-    mapCenter = null;
-
-  function doTheCreep() {
-    console.log('doing the creep');
-    // Find map center and displayed radius in miles
     var
-      bounds = map.getBounds(),
-      sw = bounds.getSouthWest(),
-      ne = bounds.getNorthEast(),
-      proximitymeters = google.maps.geometry.spherical.computeDistanceBetween(sw, ne),
-      center = bounds.getCenter(),
-      centerLat = center.hb,
-      centerLng = center.ib,
-      ajaxOptions = {
-        type     : 'GET',
-        url      : '/ham-creeper/find',
-        data     : { 'lat' : centerLat, 'lng' : centerLng, 'radius' : proximitymeters },
-        dataType : 'json'
-      };
+        $ = window.jQuery,
+        $window = $(window),
+        $doc = $(document),
+        google = window.google,
+        hams = [],
+        map = null,
+        mapCenter = null,
+        infoWindow;
 
-    $.ajax(ajaxOptions)
-      .done(function (data, textStatus, jqXHR) {
-        $.each(data.callsigns, function (callsign, data) {
-          if (typeof hams[callsign] === 'undefined') {
-            var location = new google.maps.LatLng(parseFloat(data.Location.lat), parseFloat(data.Location.lng));
-            hams[callsign] = (new google.maps.Marker({ map: map, position: location }));
-          }
-        });
-      });
-  }
+    function doTheCreep() {
+        console.log('doing the creep');
 
-  function centerMapOnUser() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        map.setCenter(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
-        map.setZoom(10);
-      });
+        $doc.startLoading();
+
+        // Find map center and displayed radius in miles
+        var
+            bounds = map.getBounds(),
+            sw = bounds.getSouthWest(),
+            ne = bounds.getNorthEast(),
+            proximitymeters = google.maps.geometry.spherical.computeDistanceBetween(sw, ne),
+            center = bounds.getCenter(),
+            centerLat = center.hb,
+            centerLng = center.ib,
+            ajaxOptions = {
+                type     : 'GET',
+                url      : '/ham-creeper/find',
+                data     : { 'lat' : centerLat, 'lng' : centerLng, 'radius' : proximitymeters },
+                dataType : 'json'
+            };
+
+        $.ajax(ajaxOptions)
+            .done(function (data, textStatus, jqXHR) {
+                $.each(data.callsigns, function (callsign, data) {
+                    if (typeof hams[callsign] === 'undefined') {
+                        var location = new google.maps.LatLng(parseFloat(data.Location.lat), parseFloat(data.Location.lng));
+                        hams[callsign] = (new google.maps.Marker({ map: map, position: location }));
+                        google.maps.event.addListener(hams[callsign], 'click', function () {
+                            infoWindow.setContent( '<a href="/call/' + callsign + '/">+' + callsign + '</a>')
+                            infoWindow.open(map, hams[callsign]);
+                        });
+                    }
+                });
+
+                $doc.stopLoading();
+            });
     }
-  }
+
+    function centerMapOnUser() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                map.setCenter(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+                map.setZoom(11);
+            });
+        }
+    }
 
 
-  function creeperInit() {
-    // bring the map into scope
-    map = window.logbookMap;
+    function creeperInit() {
+        // bring the map into scope
+        map = window.logbookMap;
+        infoWindow = new google.maps.InfoWindow();
 
-    //centerMapOnUser();
+        centerMapOnUser();
 
-    google.maps.event.addListener(map, 'idle', doTheCreep);
+        google.maps.event.addListener(map, 'idle', doTheCreep);
 
-  }
+        // Close info windows when you click anywhere on the map
+        google.maps.event.addListener( map, 'click', function () {
+            infoWindow.open( null, null );
+        });
+    }
 
-  $(function () {
+    $(function () {
+        // Initialize when the map has loaded
+        $window.bind('logbookmaploaded', creeperInit);
+    });
 
-    // Initialize when the map has loaded
-    $window.bind('logbookmaploaded', creeperInit);
-
-  });
-
-})(window);
+})(window, document);
